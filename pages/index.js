881c0f46bc0fd1a4f65c720e803c1ec576562caf
Rodrigo Trespalacios <rodrigo@wevolver.com>
2018-11-08 23:21:22 -0500

    animations

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import PostPreview from '../components/PostPreview'
 import Hero from '../components/Hero'
 import Header from '../components/Header'
+import { Spring, config, Transition } from 'react-spring'
 
 export default withPosts( ({ posts }) => {
   const sliced = posts.sort(sortByDate).slice(0, 1)
@@ -14,16 +15,26 @@ export default withPosts( ({ posts }) => {
         siteTitle="Rodrigo Trespalacios Blog"
         description="Rodrigo Trespalacios Blog"
       />
-      <Hero />
-      <h3 style={{fontWeight: 700, textTransform: 'uppercase', marginBottom: 30}}>Latest Posts</h3>
+        <Spring
+          // config={config.molasses}
+          from={{ transform: 'translate3d(0,-40px,0)', opacity: 0 }}
+          to={{ transform: 'translate3d(0, 0, 0)', opacity: 1}}>
+          {props => 
+            <div style={props}><Hero /></div>
+          }
+        </Spring>
       <section className="post-list">
-      {
-        sliced.map(post =>
-          <PostPreview
-            {...post.data}
-          />
-        )
-      }
+        <Transition
+          items={sliced} keys={sliced => sliced.key}
+          from={{ transform: 'translate3d(0,-40px,0)', opacity: 0 }}
+          enter={{ transform: 'translate3d(0,0px,0)', opacity: 1 }}
+          leave={{ transform: 'translate3d(0,-40px,0)', opacity: 0  }}>
+          {post => props =>
+            <div style={props}><PostPreview
+              {...post.data}
+            /></div>
+          }
+        </Transition>
       </section>
     </main>
   )
--- pages/post.js
+++ pages/post.js
@@ -5,26 +5,32 @@ import dayjs from 'dayjs'
 import Link from 'next/link'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Spring } from 'react-spring'
 
 export default withPost(({ post }) => (
-  <main>
-    <Header
-      siteTitle="Rodrigo Trespalacios Blog"
-      description={post.data.title}
-    />
-    <Link prefetch href="/">
-      <a href="/">← Back to Posts</a>
-    </Link>
-    <header>
-      <h1>{post.data.title}</h1>
-    </header>
-    <section>
-      <Content {...post} />
-    </section>
-    <hr style={{marginBottom: 10}}/>
-    <Link prefetch href="/">
-      <a href="/">← Back to Posts</a>
-    </Link>
-    <Footer />
-  </main>
+  <Spring
+    from={{ transform: 'translate3d(0,-40px,0)', opacity: 0 }}
+    to={{ transform: 'translate3d(0, 0, 0)', opacity: 1}}>
+    {props => 
+    <main style={props}>
+      <Header
+        siteTitle="Rodrigo Trespalacios Blog"
+        description={post.data.title}
+      />
+      <Link prefetch href="/">
+        <a href="/">← Back to Posts</a>
+      </Link>
+      <header>
+        <h1>{post.data.title}</h1>
+      </header>
+      <section>
+        <Content {...post} />
+      </section>
+      <hr style={{marginBottom: 10}}/>
+      <Link prefetch href="/">
+        <a href="/">← Back to Posts</a>
+      </Link>
+      <Footer />
+    </main>
+  }</Spring>
 ))
